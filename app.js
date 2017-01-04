c03e6fa826666a71296bf916dac6935e5548c1cb
ulrik einarson <torvalde@gmail.com>
2017-01-04 01:37:06 +0100

    successful payment

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const fs = require('fs');
 const mkdirp = require('mkdirp');
 const path = require('path');
 
-let siteData = {};
-
+let siteData = {country:'no',client:'Mjolfjell 1.0.0',hotelId:'fa940cf7-b7dd-4ec5-a52c-5425ff69313b'};
 let logo, image;
 
 
@@ -75,7 +74,7 @@ var initialize = fetch('https://demo.mews.li/api/distributor/v1/hotels/get', {
         if (roomImages.length == 0) {
             roomImages.push('http://placehold.it/400x267');
         }
-        siteData.roomCategories[room.Id] = {name: room.Name['en-US'], images: roomImages, type: room.SpaceType.toUpperCase()};
+        siteData.roomCategories[room.Id] = {name: room.Name['en-US'], images: roomImages, type: room.SpaceType.toUpperCase(), id:room.Id};
         //siteData.rooms.push({id:room.Id,name:room.Name['en-US'],images:roomImages, type:room.SpaceType.toUpperCase()});
     }
 
@@ -122,68 +121,6 @@ var initialize = fetch('https://demo.mews.li/api/distributor/v1/hotels/get', {
 }).catch(function (err) {
     console.error(err);
 });
-
-/*
- app.set('views', __dirname + '/templates');
- app.set('view engine', 'twig');
-
- // This section is optional and can be used to configure twig.
- app.set('twig options', {
- strict_variables: false
- });
-
- app.get('/available/:startUtc/:endUtc', function(req, res) {
- var adultCount = req.query.adult;
- var childCount = req.query.child;
- var startUtc = req.params.startUtc;
- var endUtc = req.params.endUtc;
- fetch('https://demo.mews.li/api/distributor/v1/hotels/getAvailability', {
- headers: {
- 'Accept': 'application/json',
- 'Content-Type': 'application/json'
- },
- method: "POST",
- body: JSON.stringify({
- "Client": "Mjolfjell 1.0.0",
- //"HotelId": "2b5a4f41-b367-4235-a08f-f5fb3595948a"
- "HotelId":"fa940cf7-b7dd-4ec5-a52c-5425ff69313b",
- "StartUtc": startUtc,
- "EndUtc": endUtc,
- "AdultCount":adultCount,
- "ChildCount":childCount
- })
- }).then(function(result) {
- if (result.status!==200) {
- throw result;
- }
- return result.json();
- }).then(function (result) {
- //var rate = {id: result.Rates[0].Id, name: result.Rates[0].Name["en-US"]};
- var availability = [];
- for (var i = 0; i < result.RoomCategoryAvailabilities.length; i++) {
- var rca = result.RoomCategoryAvailabilities[i];
- var room = Object.assign({},roomCategories[rca.RoomCategoryId]);
- if (room.type=="DORM") continue;
- room.available = rca.AvailableRoomCount;
- room.availabilityTypes = [];
- for (var j = 0; j < rca.RoomOccupancyAvailabilities.length; j++) {
- var roa = rca.RoomOccupancyAvailabilities[j];
- if (j==0) {
- room.price=roa.Pricing[0].Price.AveragePerNight["EUR"];
- }
- room.availabilityTypes.push({adult:roa.AdultCount,child:roa.ChildCount,price:{total:roa.Pricing[0].Price.Total["EUR"], averageNight: roa.Pricing[0].Price.AveragePerNight["EUR"]}});
- }
- //room.averagePrice = rca.Pricing[0].Price.AveragePerNight;
- //room.totalPrice = rca.Pricing[0].Price.Total;
- availability.push(room);
- }
- res.json(availability);
- }).catch(function (err) {
- console.error(err);
- res.end(500);
- });
- });
- */
 
 
 
